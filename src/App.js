7d1129d0ee25a30cc1cc3be5ff44be00712c6efa
DavidIoner <davidhioner@gmail.com>
2022-01-14 00:26:34 -0300

    task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import './App.css'; // This is the only way to import CSS files in React
+import './App.css' // This is the only way to import CSS files in React
+import Tasks from './components/Tasks'
 
 const App = () => { 
-  // var message = "Hello World!";
-  const [message, setMessage] = React.useState("Hello World!")
+  const [tasks, setTasks] = React.useState([
+    {
+      id: 1,
+      text: 'Vai',
+      completed: false
+    },
+    {
+      id: 2,
+      text: 'Toma',
+      completed: false
+    },
+    {
+      id: 3,
+      text: 'No cu',
+      completed: false
+    }
+  ])
+  
   return (
     <div>
-      <div className="caxota">{message}
-        <button  onClick={() => setMessage('roula')}className="button">test</button>
+      <div className="caxota">
+        <Tasks tasks={tasks} /> 
+
       </div>
     </div>
+    
   )
 }
 
